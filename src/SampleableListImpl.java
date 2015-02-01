49661ea30b10e8d743ca58d3a4342c6ba73fb157
BasilMason <basilmason@hotmail.com>
2015-02-01 14:19:42 +0000

    SampleableList final comments and testing

--- src/SampleableListImpl.java
+++ src/SampleableListImpl.java
@@ -2,21 +2,20 @@
  * Created by Basil on 28/12/2014.
  *
  * implementation of sampleable list based on array structure
+ * Status: Final.
  */
 public class SampleableListImpl extends ArrayList implements SampleableList {
 
-    /**
-     * Returns a list containing the first, third, fifth...
-     * items of this list, or an empty list if the list is empty.
-     *
-     * @return a list containing the first, third, fifth... items of this list
+    /** {@inheritDoc}
+     * @return returns odd elements of list
      */
+    @Override
     public SampleableList sample() {
 
         SampleableList ret = new SampleableListImpl();  // return list
         int size = this.size(); // get size
 
-        if (size > 0) { // if list non-empty populate with odd elements of list
+        if (size > 0) { // if list non-empty populate return list with odd elements
 
             for (int i = 0; i < size; i+=2) {   // step by 2 to get odd elements
                 ret.add(this.get(i).getReturnValue());
